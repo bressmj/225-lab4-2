708df6382a287c1945dd1d5548e33a5fa19955ec
bressmj <159402329+bressmj@users.noreply.github.com>
2025-05-18 23:08:26 -0500

    Update test_html_elements.py

--- test_html_elements.py
+++ test_html_elements.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait  # Add this import
+from selenium.webdriver.support import expected_conditions as EC  # Add this import
 import unittest
 
 class TestContacts(unittest.TestCase):
